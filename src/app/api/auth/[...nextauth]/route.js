fb7d1dc843863bfe0c14f8e197c8c2e09954c3f1
Shreyansh301 <shreyanshsriv5@gmail.com>
2025-01-28 23:57:07 +0530

    updated route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -20,6 +20,26 @@
 
 
 
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   session: {
+//     strategy: "jwt", // Use JWT to manage sessions
+//   },
+// });
+
+// // This exports the handler for both GET and POST requests
+// export { handler as GET, handler as POST };
+
+
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -33,7 +53,7 @@ const handler = NextAuth({
   session: {
     strategy: "jwt", // Use JWT to manage sessions
   },
+  secret: process.env.NEXTAUTH_SECRET, // Add the secret here
 });
 
-// This exports the handler for both GET and POST requests
 export { handler as GET, handler as POST };
