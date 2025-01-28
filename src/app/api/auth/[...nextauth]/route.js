a3efdb2d8470798f5cc13c24e785f11c933dcf97
Shreyansh301 <shreyanshsriv5@gmail.com>
2025-01-29 00:38:28 +0530

    updated route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -40,6 +40,27 @@
 
 
 
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   session: {
+//     strategy: "jwt", // Use JWT to manage sessions
+//   },
+//   secret: process.env.NEXTAUTH_SECRET, // Add the secret here
+// });
+
+// export { handler as GET, handler as POST };
+
+
+
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
@@ -54,6 +75,14 @@ const handler = NextAuth({
     strategy: "jwt", // Use JWT to manage sessions
   },
   secret: process.env.NEXTAUTH_SECRET, // Add the secret here
+  cookies: {
+    // Only set the `secure` flag on cookies in production
+    sessionToken: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // secure cookies for production
+      sameSite: "lax", // Adjust this based on needs
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
